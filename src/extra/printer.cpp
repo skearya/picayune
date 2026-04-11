544ef55604b794a8deae00492c51f3fd6a8013f4
skearya <77034153+skearya@users.noreply.github.com>
2026-04-11 17:31:10 -0400

    print ast node location darker

--- src/extra/printer.cpp
+++ src/extra/printer.cpp
@@ -54,7 +54,7 @@ void Printer::printHeader(uint8_t color, std::string_view label,
   std::print(" - ");
   std::print("\033[39m");
 
-  std::print("\033[37m");
+  std::print("\033[90m");
   std::print("\033[3m");
   std::print("[{}:{}:{}]", filename, span.line, span.start + 1);
   std::print("\033[23m");
